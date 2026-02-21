2d2075db17bdf690693fd377de67f1de396de911
marslan-786 <marslansalfias@gmail.com>
2026-02-21 17:58:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const PORT = process.env.PORT || 3000;
 const roxy = require("./api/roxy");
 const msi = require("./api/msi");
 const np = require("./api/np");
+const roxy = require("./api/roxy1");
+const msi = require("./api/msi1");
+const np = require("./api/np1");
 
 app.use("/api/roxy", roxy);
 app.use("/api/msi", msi);
 app.use("/api/np", np);
+app.use("/api/roxy1", roxy1);
+app.use("/api/msi1", msi1);
+app.use("/api/np1", np1);
 
 app.get("/", (req,res)=> res.send("API RUNNING ✅"));
 
